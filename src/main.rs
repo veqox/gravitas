749e8d5ab246108ec27a4083973f98f13a160e13
veqox <sonnleitner.luki@gmail.com>
2024-09-23 22:16:22 +0200

    feat: parse headers for udp dns packets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::net;
+
+use packet::Packet;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = net::UdpSocket::bind(format!("0.0.0.0:{}", 3000)).unwrap();
+
+    let mut buf = [0; packet::PACKET_SIZE];
+
+    while let Ok((amt, src)) = socket.recv_from(&mut buf) {
+        println!("{} bytes from {}", amt, src.ip());
+        let packet = Packet::try_from(buf).unwrap();
+        println!("Header: {:?}", packet)
+    }
 }
